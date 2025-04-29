c04f010ce5c10dd566f4d4eeb18e7379cdd6d61c
Abdul Rahim Mirani <abdulrahimmirani@gmail.com>
2025-04-29 18:24:27 -0400

    Api issue resolved, API is working

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,10 @@ import os
 import requests
 from dotenv import load_dotenv
 
-load_dotenv
+load_dotenv()
 
 def fetch_call_details(call_id):
-    url = f"https://api.vapi/ai/call/{call_id}"
+    url = f"https://api.vapi.ai/call/{call_id}"
     headers = {
         "Authorization": f"Bearer {os.getenv('VAPI_API_KEY')}"
     }
