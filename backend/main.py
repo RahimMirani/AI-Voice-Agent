964d8c3ab2fb650b91db944c55db5ab2b8b419ce
Abdul Rahim Mirani <abdulrahimmirani@gmail.com>
2025-04-29 17:36:42 -0400

    API Request to Vapi

---
+++ backend/main.py
@@ -0,0 +1,16 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv
+
+def fetch_call_details(call_id):
+    url = f"https://api.vapi/ai/call/{call_id}"
+    headers = {
+        "Authorization": f"Bearer {os.getenv('VAPI_API_KEY')}"
+    }
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+fetch_call_details("")
+
